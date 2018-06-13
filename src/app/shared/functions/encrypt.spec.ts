ececa84f9ab237f6120e09aca920a7e92f6defe0
Shivam Sharma <sdsharma@ucsd.edu>
2018-06-12 17:12:13 -0700

    Encrypt/decypt tests

---
+++ src/app/shared/functions/encrypt.spec.ts
@@ -0,0 +1,15 @@
+import * as sjcl from 'sjcl';
+import { encryptField, decryptField } from './encrypt';
+
+describe('Encrypt', () => {
+  it('should encrypt data', () => {
+    let data = encryptField('hi', 'hello');
+    expect(data).toBeTruthy();
+  });
+
+  it('should decrypt encrypted data', () => {
+    let data = encryptField('hi', 'hello');
+    data = decryptField('hi', data);
+    expect(data).toEqual('hello');
+  });
+});
